30f821b4c7933a57dca17cfdf0bdd47207d54ae7
Will Wilson <wbwilsoniv@gmail.com>
2018-07-10 08:13:07 -0400

    WIP display function stuck in loop. round win logic not registering

--- scripts.js
+++ scripts.js
@@ -8,14 +8,16 @@ const form = document.querySelector('.gameBoard');
 const btn = form.querySelectorAll(".gamebtn");
 const clearBtn = document.querySelector('.clear');
 const startBtn = document.querySelector('.start');
-const btnArray = ['green',`red`,'yellow', 'blue'];
 const redSound = new Audio('sounds/red-piano-a.wav');
 const blueSound = new Audio('sounds/blue-piano-f.wav');
 const yellowSound = new Audio('sounds/yellow-piano-c.wav');
 const greenSound = new Audio('sounds/green-piano-e.wav');
+const btnArray = ['green',`red`,'yellow', 'blue'];
 let userScore = 0;
+let i = 0;
 let compArray = [];
 let userArray = [];
+let level = compArray.length;
 // let displayArray 
 
 // Game Logic Functions
@@ -36,11 +38,39 @@ function randomize() {
 function gameStart() {
   startBtn.addEventListener('click', (e) => {
       e.preventDefault();
-      showCompArray();
-      //playGame(compArray);
+      randomize();
   })
 }
-gameStart();
+
+function clearGame() {
+      clearBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        userArray.length = 0;
+        compArray.length = 0;
+        userScore = 0;
+      })
+}
+// GAME LOOP
+
+// clearGame();
+// gameStart();
+// randomize();
+// showCompArray();
+// getUserInput();
+// timedArrayCheck();
+// update scoreboard();
+// checkArrays();
+function gameLoop() {
+      clearGame();
+      gameStart();
+      randomize();
+      showCompArray();
+      getUserInput();
+      timedArrayCheck();
+      debugger;
+      showScore();
+}
+gameLoop();
 
 // function to take User input and push to userArray
 // when button click, pushes value (set as color) of button to userArray
@@ -51,56 +81,55 @@ function getUserInput() {
         if (e.target.value === 'green'){
               greenSound.play();
               userArray.push('green');
-              greenBtn.classList.add('glow');
+              greenBtn.classList.toggle('glow');
         }
         if (e.target.value === 'blue'){
               blueSound.play();
               userArray.push('blue');
-              blueBtn.classList.add('glow');
+              blueBtn.classList.toggle('glow');
         }
         if (e.target.value === 'red'){
               redSound.play();
               userArray.push('red');
-              redBtn.classList.add('glow');
+              redBtn.classList.toggle('glow');
         }
         if (e.target.value === 'yellow'){
               yellowSound.play();
               userArray.push('yellow');
-              yellowBtn.classList.add('glow');
+              yellowBtn.classList.toggle('glow');
         }
       // debugger;
-      checkArrays(userArray, compArray);
+      // checkArrays(userArray, compArray);
   });
 }
 
 
 // game logic to check userArray & compArray then call randomize function to add additional color and increase userScore by 1
+function timedArrayCheck(){
+let arrayCheck = setTimeout(function(){
+      checkArrays(userArray, compArray);
+}, 5000);
+}
+
 function checkArrays(userArray, compArray) {
-      let i = 0;
-      console.log(userArray);
-      console.log(compArray)
-      if(userArray[i] === compArray[i]) {
+      if(userArray === compArray) {
+            i+=1;
             userScore += 1;
             alert('Correct! Get ready for the next round');
+            clearInterval(showCompArray);
+            randomize();
             if(userArray.length === compArray.length && userScore >= 5){
                alert('Winner');
             };
       //else if (userArray !== compArray) {
         //    alert('You Lose. Womp Womp.');
      // }
-     compArray[i]
-     i+=1
+//      compArray[i]
+//      i+=1
   } else alert('You Lose. Womp Womp.');
 }
 // clear game function
-function clearGame() {
-      clearBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        userArray.length = 0;
-        compArray.length = 0;
-        userScore = 0;
-      })
-}
+
 
 
 // View
@@ -109,43 +138,45 @@ function clearGame() {
 // loops over array - toggles display for each color
 // code modified from https://medium.com/front-end-hacking/create-simon-game-in-javascript-d53b474a7416
 function showCompArray () {
-      let i = 0;
       let showComp = setInterval(function(){
-        playGame(compArray);
-        if (i >= compArray.length) {
+        compMoves(compArray);
+        if (i > compArray.length) {
               clearInterval(showComp);
         }
-      }, 4000)
+      }, 1000)
 }
 
-function playGame(compArray) {
-      let i = 0;
-      let randomIndex = randomize();
-      console.log(compArray[randomIndex])
-      if(compArray[randomIndex ] == 'green') {
-          greenBtn.classList.add('glow');
+function compMoves(compArray) {
+      let level = compArray.length;
+      while(level > 0){
+        if(compArray[i] === 'green') {
+          greenBtn.classList.toggle('glow');
           //set timeout
           setTimeout(function(){
-            greenBtn.classList.remove('glow');  getUserInput();
-            //   
-          }, 2000);
+            greenBtn.classList.toggle('glow');
+          }, 500);
+          level-=1;
           // if statement to end loop within getUserInput - stops loop - maybe in getUserInput
-      } else if(compArray[randomIndex ] === 'red') {
-          redBtn.classList.add('glow');
+      } else if(compArray[i] === 'red') {
+          redBtn.classList.toggle('glow');
           setTimeout(function(){
-            redBtn.classList.remove('glow');  getUserInput();  
-          }, 2000);
-      } else if(compArray[randomIndex ] === 'blue') {
-          blueBtn.classList.add('glow');
+            redBtn.classList.toggle('glow');
+          }, 500);
+          level-=1;
+      } else if(compArray[i] === 'blue') {
+          blueBtn.classList.toggle('glow');
           setTimeout(function(){
-            blueBtn.classList.remove('glow');  getUserInput();  
-          }, 2000);
-      } else if(compArray[randomIndex ] === 'yellow') {
-            yellowBtn.classList.add('glow');
+            blueBtn.classList.toggle('glow');
+          }, 500);
+          level-=1;
+      } else if(compArray[i] === 'yellow') {
+            yellowBtn.classList.toggle('glow');
             setTimeout(function(){
-                  yellowBtn.classList.remove('glow');  getUserInput();  
-                }, 2000);
+                  yellowBtn.classList.toggle('glow');
+                }, 500);
+                level-=1;
       }
+}
      // debugger;
 
 }
@@ -170,10 +201,6 @@ function playGame(compArray) {
 //       }
 // }
 // let showComp = setInterval(showCompArray, 1000);
-// function for displaying userArray
-function showUserArray(){
-      redBtn.add
-}
 // function to display userScore
 function showScore (){
       const scoreBoard = document.querySelector('.scoreboard > p');
